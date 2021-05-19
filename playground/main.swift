aba461f5c3b32e1a517eecb410c63cf8f6761715
Dion <ksundong@gmail.com>
2021-05-19 22:25:17 +0900

    feat: Swift Tour - Simple Values

--- playground/main.swift
+++ playground/main.swift
@@ -8,19 +8,71 @@
 
 import Foundation
 
-// 한 줄 주석
+print("Hello, world!") // Hello, world!
 
-/* 여러 줄 주석
- 이 줄도 주석
- 요 줄도 주석 */
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
 
-/*
- 이렇게도 쓸 수 있다.
- */
+// you don't always have to write the type explicitly.
 
-/*
- /*
- // 주석 안의 주석
- */
- 이 줄도 주석
- */
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+// Create a constant with an explicit type of Float and a value of 4.
+
+let explicitFloat: Float = 4
+
+// If you need to convert a value to a different type, explicitly make an instance of the desired type.
+
+let label = "The witdth is "
+let width = 94
+// different with Java(java can label + width)
+let widthLabel = label + String(width)
+
+// Simple way
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+// Use \() to include a floating-point calculation in a string and to include someone’s name in a greeting.
+// I don't like this experiment
+
+let someoneName = "dion"
+let point = 40.0
+let greetingMessage = "Greeting! \(someoneName) Your point is \(point)"
+print(greetingMessage)
+
+// three double quotation, multiple lines String
+
+let quotation = """
+I said "I have \(apples) apples."
+And then I said "I have \(apples + oranges) pieces of fruit."
+"""
+
+// arrays and dictionaries([])
+
+var shoppingList = ["catfish", "water", "tulips"]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+occupations["Jayne"] = "Public Relations"
+
+shoppingList.append("blue paint")
+print(shoppingList)
+
+// empty array and dictionary
+
+let emptyArray = [String]()
+let emptyDictionary = [String: Float]()
+
+// If type information can be inferred, you can write an empty array as [] and an empty dictionary as [:]—for example, when you set a new value for a variable or pass an argument to a function.
+
+shoppingList = []
+occupations = [:]
